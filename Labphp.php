04eb5b1c65fc77e480b6cc8fd3dee1c1eafd514c
Sayma Trina <trinasayma5191@gmail.com>
2025-04-28 14:53:51 +0600

    Add files via upload
    update1

---
+++ Labphp.php
@@ -0,0 +1,97 @@
+<?php
+
+$length=10;
+$width=15;
+$Rectangle= $length * $width;
+
+echo"Task1 :-";
+echo"The area of Rectangle{$Ractangle} <br>";
+$perimeter=2*($length + $width);
+echo" perimeter {$perimeter}";
+
+echo"Task2 :-";
+$amount=1000;
+
+$VAT=0.15* $amount;
+echo "VAT :{ $VAT}";
+
+echo"Task3 :-";
+
+$number=20;
+
+if($number%2==0)
+{
+    echo" 20 is EVEN number";
+}
+
+else{
+    echo" 20 is ODD number";
+}
+
+echo"Task4 :-";
+ $num1=10;
+ $num2=20;
+ $num3=30;
+
+ if($num1>$num2 && $num1> $num3)
+ {
+    echo" Num1 is largest Number";
+ }
+else if($num2>$num1 && $num2> $num3)
+{
+    echo"Num2 is largest number";
+}
+
+else{echo"Num3 is largest Number";}
+
+echo"Task5 :-";
+
+for($i=10; $i<100 ;$i++)
+{
+ if($i%2!=0)
+ {
+    echo"i <br>";
+ }
+ echo"Task6 :-";
+
+
+
+ $src_num= 10;
+$arr=[1,10,1000];
+for($j= 0; $j< $arr.$length; $j++)
+
+{
+    if($src_num==$arr[$j])
+    {
+        echo "the Number found in array";
+    }
+
+
+    else{echo"Number not found in array";}
+}
+echo"Task7 :-";
+
+$star="*";
+ for ($i=0; $i<3 ; $i++)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+?>
